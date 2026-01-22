87a50feb1cca191b8a2e64c3d11701c630e5a463
Delaien <julien@delaforge.eu>
2026-01-22 14:35:07 +0100

    petit fix

--- src/test/java/hesias/selenium/practices/correction/tests/BaseTest.java
+++ src/test/java/hesias/selenium/practices/correction/tests/BaseTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -20,8 +21,26 @@ public class BaseTest {
 
     @BeforeEach
     protected void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        String chromeVersion = System.getenv("CHROME_VERSION");
+        ChromeOptions options = new ChromeOptions();
+        String chromeBinary = System.getenv("CHROME_BIN");
+        if (chromeBinary != null && !chromeBinary.isBlank()) {
+            options.setBinary(chromeBinary);
+        }
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--remote-debugging-port=9222");
+        options.addArguments("--no-first-run");
+        options.addArguments("--no-default-browser-check");
+        options.addArguments("--window-size=1920,1080");
+        WebDriverManager chromedriver = WebDriverManager.chromedriver();
+        if (chromeVersion != null && !chromeVersion.isBlank()) {
+            chromedriver.browserVersion(chromeVersion);
+        }
+        driver = chromedriver.capabilities(options).create();
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
