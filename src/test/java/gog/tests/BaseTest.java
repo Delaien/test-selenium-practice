455311a8f3d8c19bd242d6c7c5f842f1ee20484b
Delaien <julien@delaforge.eu>
2026-01-22 11:44:50 +0100

    fix le retour

--- src/test/java/gog/tests/BaseTest.java
+++ src/test/java/gog/tests/BaseTest.java
@@ -30,9 +30,15 @@ public class BaseTest {
     protected void setUp() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        String chromeBinary = System.getenv("CHROME_BIN");
+        if (chromeBinary != null && !chromeBinary.isBlank()) {
+            options.setBinary(chromeBinary);
+        }
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
         options.addArguments("--window-size=1920,1080");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
